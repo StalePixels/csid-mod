1f79129abcd44b54bdf7da883912f1e5f7a0f3a8
D. Rimron-Soutter <darran@xalior.com>
2019-09-17 21:28:41 +0100

    Make demo demo something!

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -3,6 +3,7 @@
 # First, I need some stuff, trust me, I'm trust worthy...
 sudo /usr/bin/apt-get install libsdl-sound1.2-dev libsql-sound1.2 build-essential
 
+# Install
 cd /tmp
 git clone https://github.com/StalePixels/csid-mod
 cd csid-mod
@@ -11,7 +12,11 @@ make
 # Now I need to put it into place, but you should still trust me...
 sudo cp build/csidl build/csid /usr/bin
 
-curl http://zx.xalior.com/monty.sid -q | csidl
+#Clean up
+rm -rf /tmp/csid-mod
+
+# THE DEMO
+curl http://zx.xalior.com/monty.sid | csidl
 
 # and NEVER trust a shellscript from someone you don't trust, okay!
 echo DONE!
