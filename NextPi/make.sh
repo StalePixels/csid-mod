e9ea1cf594692c996d6ea512801afcfff0dc331e
D. 'Xalior' Rimron-Soutter <darran@xalior.com>
2019-09-22 13:14:05 +0100

    CI hooks

---
+++ NextPi/make.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Ensure we have everything we need
+apt-get install build-essential -y
+
+# This package is so simple we don't even use ./configure, but if it did, use /usr
+echo "We don't need no stinking.... ./configure --prefix=/usr"
+
+# Now build
+make
+
