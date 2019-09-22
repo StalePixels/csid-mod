521adb58bbd71c14ae2944c81c414742770eb7d7
D. 'Xalior' Rimron-Soutter <darran@xalior.com>
2019-09-22 14:07:34 +0100

    force sudo for packages

--- NextPi/install.sh
+++ NextPi/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #Nothing grand, but allows the CI to wrap this operation to build custom packages for NextPi
-make install
+cp build/csid /usr/bin
+cp build/csidl /usr/bin
--- NextPi/make.sh
+++ NextPi/make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Ensure we have everything we need
-apt-get install build-essential -y
+sudo apt-get install build-essential -y
 
 # This package is so simple we don't even use ./configure, but if it did, use /usr
 echo "We don't need no stinking.... ./configure --prefix=/usr"
